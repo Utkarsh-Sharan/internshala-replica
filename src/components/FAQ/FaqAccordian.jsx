d50045877574e6c369e0cc761793ae904e461d0d
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-05-16 17:47:57 +0530

    add faq heading

--- src/components/FAQ/FaqAccordian.jsx
+++ src/components/FAQ/FaqAccordian.jsx
@@ -3,10 +3,13 @@ import FaqItem from "./FaqItem";
 
 const FaqAccordian = () => {
   return (
-    <section className="max-w-2xl mx-auto mt-10 bg-white shadow-md rounded-md">
-        {faqs.map((faq, idx) => (
-            <FaqItem key={idx} question={faq.question} answer={faq.answer} />
-        ))}
+    <section className="flex flex-col justify-center items-center">
+        <h4 className="border border-blue-500 rounded-md px-4 py-1 text-blue-500 mt-10">Frequently Asked Questions</h4>
+        <section className="max-w-2xl mx-auto mt-10 bg-white shadow-md rounded-md">
+            {faqs.map((faq, idx) => (
+                <FaqItem key={idx} question={faq.question} answer={faq.answer} />
+            ))}
+        </section>
     </section>
   )
 }
