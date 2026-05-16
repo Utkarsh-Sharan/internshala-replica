244fadbbedf2e1bb091040d5977d32f001459502
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-05-16 17:24:04 +0530

    create faq item component

---
+++ src/components/FAQ/FaqAccordian.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FaqAccordian = () => {
+  return (
+    <div>FaqAccordian</div>
+  )
+}
+
+export default FaqAccordian
---
+++ src/components/FAQ/FaqItem.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const FaqItem = ({question, answer}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <article className='border-b border-gray-200'>
+        <button
+            onClick={() => setIsOpen(prev => !prev)}
+            className='w-full flex justify-between items-center py-4'
+        >
+            <span className='font-medium text-gray-800'>{question}</span>
+            <span className='ml-2 text-gray-500'>{isOpen ? "-" : "+"}</span>
+        </button>
+
+        <article className={`transition-all duration-300 ease-in-out overflow-hidden ${
+            isOpen ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
+        }`}>
+            <p className='text-gray-600 px-2 pb-4'>{answer}</p>
+        </article>
+    </article>
+  )
+}
+
+export default FaqItem
